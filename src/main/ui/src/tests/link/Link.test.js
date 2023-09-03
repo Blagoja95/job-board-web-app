22f232886bb520bcdd59803846a64e2d75ca5652
Boris <boris.blagojevicc@hotmail.com>
2023-09-03 09:50:03 +0200

    Basic tests
    Update for Users component, if no user list is empty show no users msg

--- src/main/ui/src/tests/link/Link.test.js
+++ src/main/ui/src/tests/link/Link.test.js
@@ -1,25 +1,29 @@
 import renderer from 'react-test-renderer';
 import Link from '../../testExamples/link/Link';
 
-it('Class is changed when hovered', () => {
-	const cmp = renderer.create(
-		<Link page="http://www.facebook.com">Facebook</Link>,
-	);
+describe("TestExamples", () => {
+	describe("Link", () => {
+		it('Class is changed when hovered', () => {
+			const cmp = renderer.create(
+				<Link page="http://www.facebook.com">Facebook</Link>,
+			);
 
-	let tree = cmp.toJSON();
-	expect(tree).toMatchSnapshot();
+			let tree = cmp.toJSON();
+			expect(tree).toMatchSnapshot();
 
-	renderer.act(() => {
-		tree.props.onMouseEnter();
+			renderer.act(() => {
+				tree.props.onMouseEnter();
+			});
+
+			tree = cmp.toJSON();
+			expect(tree).toMatchSnapshot();
+
+			renderer.act(() => {
+				tree.props.onMouseLeave();
+			});
+
+			tree = cmp.toJSON();
+			expect(tree).toMatchSnapshot();
+		});
 	});
-
-	tree = cmp.toJSON();
-	expect(tree).toMatchSnapshot();
-
-	renderer.act(() => {
-		tree.props.onMouseLeave();
-	});
-
-	tree = cmp.toJSON();
-	expect(tree).toMatchSnapshot();
 });
